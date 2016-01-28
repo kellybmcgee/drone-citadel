c987cf3b10bd3a4b72f824ee0079e782cea29d67
MxMcG <mmcgeevibes@gmail.com>
2016-01-28 15:17:42 -0800

    add contact page

--- client/templates/about.js
+++ client/templates/about.js
@@ -1,5 +1,6 @@
 Template.about.events({
   'click .white-arrow': function (e) {
+
     var target = e.currentTarget.nextElementSibling;
     var thisTarget = $(target).attr("id");
     $("#" + thisTarget).toggle(
@@ -8,5 +9,27 @@ Template.about.events({
     },  function (target) {
           $(this).removeClass( "selected" );
     });
+    var targetArw = e.currentTarget.id;
+    $("#" + targetArw).css({
+      "transform": "rotate(90deg)",
+      "transition": "transform 1.5s"
+    });
+
+    // .toggle(
+    //     function (target) {
+    //       $(this).addClass( "rotateArw" );
+    // },  function (target) {
+    //       $(this).removeClass( "rotateArw" );
+    // });
+// function (target) {
+//   $("#" + targetArw).css({
+//   "transform": "rotate(90deg)",
+//   "transition": "transform 1.5s"
+//   });
+// }, function (target) {
+//   $("#" + targetArw).css({
+//   "transform": "rotate(-90deg)",
+//   "transition": "transform 1.5s"
+//   });
   }
 });
